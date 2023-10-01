d2d9b552b2968d0abb9955ddf65a8908fffff478
benberton <91513576+benberton@users.noreply.github.com>
2023-10-01 13:06:20 -0700

    basic solution for this weeks hw

--- nim_player.py
+++ nim_player.py
@@ -1,11 +1,35 @@
+# Author: Ben Berton
+# Desc: This file contains the NimPlayer class which is used to play the game of nim.
+# Date: 10/1/2023
 class NimPlayer:
     def __init__(self) -> None:
         pass
 
     # takes in a board state and returns a move
     def play(self, board):
-        for i in range(len(board)):
-            if board[i] > 0:
-                board[i] -= 1
-                return board
+        if (self.getNimSum(board) == 0):
+            for i in range(len(board)):
+                if board[i] > 0:
+                    board[i] -= 1
+                    return board
+        else:
+            for i in range(len(board)):
+                boardSize = board[i]
+                for j in range(boardSize):
+                    board[i] = j
+                    if (self.getNimSum(board) == 0):
+                        return board
+                board[i] = boardSize
         return board
+        
+    def getNimSum(self, board):
+        nimSum = 0
+        for i in range(len(board)):
+            nimSum ^= board[i]
+        return nimSum
+    
+    def deepCopy(self, board):
+        newBoard = []
+        for i in range(len(board)):
+            newBoard.append(board[i])
+        return newBoard
